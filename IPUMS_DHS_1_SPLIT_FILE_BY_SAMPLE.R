86f1e1f9335433df4832e823dd919c541f49fd58
Juan Galeano <jgaleano@ced.uab.es>
2022-11-25 09:28:31 +0100

    Update IPUMS_DHS_1_SPLIT_FILE_BY_SAMPLE.R

--- IPUMS_DHS_1_SPLIT_FILE_BY_SAMPLE.R
+++ IPUMS_DHS_1_SPLIT_FILE_BY_SAMPLE.R
@@ -6,7 +6,7 @@
 
 # 1. WD ####
 
-setwd("G:\\Shared drives\\CORESIDENCE\\WP2_DATA\\2_1_Input Data\\CENSUS_IPUMS\\A1_IMPUS_MOTHERS");getwd()
+setwd("G:\\Shared drives\\CORESIDENCE\\WP2_DATA\\2_1_Input Data\\DHS_IPUMS\\A1_IPUMS_DHS_MOTHERS");getwd()
 
 # 2. Libraries ####
 
@@ -15,23 +15,10 @@ library(readxl)
 
 # 3. Import data ####
 
-ddi <- read_ipums_ddi("ipumsi_00115.xml")
+ddi <- read_ipums_ddi("idhs_00007.xml")
 data <- read_ipums_micro(ddi)
 head(data,10) # VARIABLES HAVE THEIR LABELS
 
-# 4. Mini Test ####
-
-mini_test<-data[c(1:100),]
-levels(mini_test$COUNTRY)
-head(data,10)
-class(mini_test$SAMPLE)
-mini_test$COUNTRY2<-as_factor(mini_test$COUNTRY)
-library(readxl)
-codes_countries <- read_excel("G:\\Shared drives\\CORESIDENCE\\WP2_DATA\\2_1_Input Data\\CENSUS_IPUMS\\A1_IMPUS_MOTHERS\\codes_countries.xlsx")
-mini_test<-as.data.frame(mini_test)
-codes_countries<-as.data.frame(codes_countries)
-mini_test <- data.frame(mini_test, codes_countries[match(mini_test[,"COUNTRY2"], codes_countries[,"country"]),c("iso3")])
-colnames(mini_test)[213]<-"ISO_CODE"
 
 # 5. Read codes iso countries and add it to ipums data ####
 
@@ -45,6 +32,14 @@ colnames(data)[213]<-"ISO_CODE"
 # 6. Split data by SAMPLE and save file by country and year ####
 
 IPUMS_DHS_list<-split(data , f = data$SAMPLE )
+
+not_all_na <- function(x) any(!is.na(x))
+
+IPUMS_DHS_list<-lapply(IPUMS_DHS_list, function(df) {
+  df<-df %>% select(where(not_all_na))
+  df
+})
+
 save(IPUMS_DHS_list, file="G:\\Shared drives\\CORESIDENCE\\WP2_DATA\\2_1_Input Data\\DHS_IPUMS\\IPUMS_DHS_list.Rdata")
 
 setwd("G:\\Shared drives\\CORESIDENCE\\WP2_DATA\\2_1_Input Data\\DHS_IPUMS\\")
